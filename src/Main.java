6749b469b3637f1da5c5ac68ede67fa254b8a942
Denis <demaz@bk.ru>
2022-07-28 20:35:46 +0700

    task 1 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        homework_6();
+
     }
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    public static void homework_6() {
+        int[] arr = generateRandomArray();
+
+        //task1
+
+        int sumPerMonth = 0;
+
+        for (int sumPerDay: arr) {
+            sumPerMonth += sumPerDay;
+        }
+        System.out.println("Сумма трат за месяц составила " + sumPerMonth + " рублей");
+
+
+    }
+
+
+
 }
