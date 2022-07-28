2758c4d96f960e802cc20ce44418d2cef37e85b3
Denis <demaz@bk.ru>
2022-07-28 21:34:18 +0700

    task 4 done

--- src/Main.java
+++ src/Main.java
@@ -41,6 +41,13 @@ public class Main {
         int amountOfDays = 30;
         double averageSum = (double) sumPerMonth / amountOfDays;
         System.out.println("Средняя сумма трат за месяц составила " + averageSum + " рублей");
+
+        //task4
+
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
 
 
